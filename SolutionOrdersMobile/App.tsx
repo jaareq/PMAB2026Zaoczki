fa1a7f5aa50a67a7da16c17ece2e8e65f7f96266
Jarosław <jaareq@interia.pl>
2026-05-08 20:03:48 +0200

    feat(Lecture2): Component with properties

--- SolutionOrdersMobile/App.tsx
+++ SolutionOrdersMobile/App.tsx
@@ -1,44 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { View, ScrollView, StyleSheet, useColorScheme, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Greeting from './src/components/Greeting';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
+function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
+    <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+      <ScrollView>
+        <Greeting name="Anna" age={25} />
+        <Greeting name="Piotr" isVip={true} />
+        <Greeting name="Kasia" age={30} isVip={true} />
+        <Greeting name="Jan" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f5f5f5',
   },
 });
 
