8acef964d2b8a243aa1a935a800e574df97dcec4
Jarosław <jaareq@interia.pl>
2026-05-08 20:48:06 +0200

    feat(Counter): add Counter component with increment, decrement, and reset functionality feat(ItemList): create ItemList component to display a list of items with prices

--- SolutionOrdersMobile/App.tsx
+++ SolutionOrdersMobile/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, ScrollView, StyleSheet, useColorScheme, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Greeting from './src/components/Greeting';
+import Counter from './src/components/Counter';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -9,6 +10,7 @@ function App(): React.JSX.Element {
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView>
+        <Counter />
         <Greeting name="Anna" age={25} />
         <Greeting name="Piotr" isVip={true} />
         <Greeting name="Kasia" age={30} isVip={true} />
